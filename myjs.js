2d209891242e70ed688a7d1146b2d466929c891b
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-30 22:10:25 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -8,7 +8,9 @@ function imgchange2()
 document.getElementById('logo').src = './image/project.jpg';
 }
 
-function txtchange1()
-{
- document.getElementById('txt1').innerHTML='イタリア';
-}
+fanction hyouji(){
+ //表示する文字
+var str="イタリア"
+//テキストボックスの文字数
+var cnt=document.timer.moji.value.length;
+//文字が全部表示されているか確認 
