ed40aed90b65163d962c4673b9ccc340f361fdbd
kooota0818 <131091962+kooota0818@users.noreply.github.com>
2024-01-30 23:12:38 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -4,7 +4,7 @@ var str="イタリア";
 //テキストボックスの文字数
 var cnt=document.timer.moji.value.length;
 //文字が全部表示されているか確認 
-if(cnt<6){
+if(cnt<5){
 //現在より1文字多い切り出して表示
 document.timer.moji.value=str.substr(0,cnt+1);}
 else{
